85ecdb86e7c50b4befc82e4021b0bd5cc1dc9fec
Felipe Garcia Gonzalez <a21490838@itmexicali.edu.mx>
2025-03-20 15:57:58 -0700

    cambiosIDparaPaquetes

--- PUnidad3/JS/PaqAdi.js
+++ PUnidad3/JS/PaqAdi.js
@@ -1,6 +1,16 @@
-function seleccionarPaquete(nombre, detalles) {
-    console.log("Paquete seleccionado:", nombre, detalles); // Verifica si se ejecuta
+function seleccionarPaquete(nombre, detalles, precio, tipo) {
+    console.log("Paquete seleccionado:", nombre, detalles, precio, tipo); // Verifica si se ejecuta
     localStorage.setItem('paqueteNombre', nombre);
     localStorage.setItem('paqueteDetalles', detalles);
+    localStorage.setItem('paquetePrecio', precio);//Separar el valor del precio en variable int para poder sumarlo y agregar $ dlls fuera de variable como string
+    localStorage.setItem('paqueteTipo', tipo);
     window.location.href = 'infoAdicional.html'; // Redirigir a la página Adicionales
 }
+
+detalleDeDiam1 = document.getElementById('detallDiam1').textContent;
+detalleDeDiam2 = document.getElementById('detallDiam2').textContent;
+detalleDeDiam3 = document.getElementById('detallDiam3').textContent;
+detalleDeEnsue = document.getElementById('detallEnsue').textContent;
+detalleDeCinem = document.getElementById('detallCinem').textContent;
+detalleDeEstre = document.getElementById('detallEstre').textContent;
+detalleDeConce = document.getElementById('detallConce').textContent;
